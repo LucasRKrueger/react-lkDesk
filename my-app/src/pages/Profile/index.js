e31194148ac92f319a924692ba11f4d911331ac4
Lucas Krueger <lucasrkthd@gmail.com>
2021-08-17 13:49:42 -0300

    profile page

--- my-app/src/pages/Profile/index.js
+++ my-app/src/pages/Profile/index.js
@@ -1,9 +1,17 @@
+import { useState, useContext } from 'react';
 import './profile.css';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { FiSettings } from 'react-icons/fi';
+import { FiSettings, FiUpload } from 'react-icons/fi';
+import { AuthContext } from '../../contexts/auth';
+import avatar from '../../assets/avatar.png'
 
 const Profile = () =>{
+    const {user, signOut} = useContext(AuthContext);
+    const [name, setName] = useState(user && user.name);
+    const [email, setEmail] = useState(user && user.email);
+    const [avatarUrl, setAvatar] = useState(user && user.avatarUrl);
+
     return (
         <div>
             <Header/>
@@ -12,6 +20,32 @@ const Profile = () =>{
                     <FiSettings size={25} />
                 </Title>
 
+                <div className="container">
+                    <form className="form-profile">
+                        <label className="label-avatar">
+                            <span>
+                                <FiUpload color="#fff" size={25}/>
+                            </span>
+                            <input type="file" accept="image/*"/><br/>
+                            {avatarUrl === null ?
+                            <img src={avatar} width="250" height="250" alt="User Photo"/> 
+                            :
+                            <img src={avatarUrl} width="250" height="250" alt="User Photo"/>
+                            }
+                        </label>
+
+                        <label>Name</label>
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+
+                        <label>Email</label>
+                        <input type="text" value={email} onChange={(e) => setName(e.target.value)} disabled={true}/>
+
+                        <button type="submit">Save</button>
+                    </form>
+                </div>
+                <div className="container">
+                    <button className="logout-btn" onClick={() => signOut()}>Logout</button>
+                </div>
             </div>
         </div>
     )

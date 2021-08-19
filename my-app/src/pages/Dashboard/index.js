7a2b3146b84d2e1815c0aacc8ed9ec4a79ce76e7
Lucas Rodrigo Krueger <lucasrkthd@gmail.com>
2021-08-19 00:42:01 -0300

    search more button

--- my-app/src/pages/Dashboard/index.js
+++ my-app/src/pages/Dashboard/index.js
@@ -1,12 +1,90 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import './dashboard.css'
 import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import firebase from "../../services/firebaseConnection"
+import { format } from "date-fns";
+
+const listRef = firebase.firestore().collection('tickets').orderBy('createdOn', 'desc');
 
 const Dashboard = () =>{
-    const [tickets, setTickets] = useState([1]);
+    const [tickets, setTickets] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(false);
+    const [lastDocs, setLastDocs] = useState();
+    useEffect(() => {
+
+        loadTickets();
+
+        return () => {
+
+        }
+    }, [])
+
+    async function loadTickets(){
+        await listRef.limit(5)
+        .get()
+        .then((snapshot) => {
+            updateState(snapshot)
+        })
+        .catch((err) =>{
+            setLoadingMore(false)
+        })
+        setLoading(false);
+    }
+
+    async function handleMore(){
+        setLoadingMore(true);
+        await listRef.startAfter(lastDocs).limit(5)
+        .then((snapshot) =>{
+            updateState(snapshot);
+        })
+    }
+
+    async function updateState(snapshot){
+        const isCollectionEmpty = snapshot.size === 0;
+        if(!isCollectionEmpty){
+            let list = [];
+
+            snapshot.forEach((doc) =>{
+                list.push({
+                    id: doc.id,
+                    title: doc.data().title,
+                    customer: doc.data().customer,
+                    customerId: doc.data().customerId,
+                    createdOn: format(doc.data().createdOn.toDate(), 'dd/MM/yyyy'),
+                    status: doc.data().status,
+                    complement: doc.data().complement
+                })
+            })
+            const lastDoc = snapshot.docs[snapshot.docs.length -1];
+            setTickets(tickets => [...tickets, ...list]);
+            setLastDocs(lastDoc);
+        } else{
+            setIsEmpty(true);
+        }
+        setLoadingMore(false);
+    }
+
+    if(loading){
+        return(
+            <div>
+                <Header/>
+                <div className="content">
+                    <Title name="Services">
+                        <FiMessageSquare size={25} />
+                    </Title>
+
+                    <div className="container dashboard">
+                        <span>Searching Tickects...</span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div>
@@ -41,11 +119,13 @@ const Dashboard = () =>{
                                 </tr>                                
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td data-label="Customer">Lucas Krueger</td>
-                                    <td data-label="Title">Test</td>
-                                    <td data-label="Status"><span className="badge" style={{backgroundColor: '#5cb85c'}}>Open</span></td>
-                                    <td data-label="CreatedOn">17/08/2021</td>
+                                {tickets.map((item,index) =>{
+                                    return(
+                                <tr key={index}>
+                                    <td data-label="Customer">{item.customer}</td>
+                                    <td data-label="Title">{item.title}</td>
+                                    <td data-label="Status"><span className="badge" style={{backgroundColor: '#5cb85c'}}>{item.status}</span></td>
+                                    <td data-label="CreatedOn">{item.createdOn}</td>
                                     <td data-label="#">
                                         <button className="action" style={{backgroundColor: '#3586f6'}}>
                                             <FiSearch color="#fff" size={17}/>
@@ -55,8 +135,13 @@ const Dashboard = () =>{
                                         </button>
                                     </td>
                                 </tr>
+                                    )
+                                })}
+                                
                             </tbody>
                         </table>
+                        {loadingMore && <h3 style={{textAlign: 'center', marginTop: 15}}>Searching data...</h3>}
+                       { !loadingMore && !isEmpty && <button className="btn-more" onClick={handleMore}>Search More</button>}
                     </>
                 )}
                             
